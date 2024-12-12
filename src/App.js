0d336836cb54bee9b661897eb51dfa43b04b2d98
Bala <balakrishna.malipeddi@gmail.com>
2024-12-12 13:50:42 +0530

    commits saved.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import SignUpForm from './components/SignUpForm';
-
+import LoginForm from './components/LoginForm';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>E-Commerce User Registration</h1>
-        <SignUpForm />
-      </header>
-    </div>
-  );
+    const [loggedInUser, setLoggedInUser] = useState(null);
+
+    const handleLogout = () => {
+        setLoggedInUser(null);
+    };
+
+    const isAdmin = loggedInUser === 'admin'; 
+    
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>myshow4all</h1>
+                {!loggedInUser ? (
+                    <LoginForm setLoggedInUser={setLoggedInUser} />
+                ) : (
+                    <>
+                        <p>Welcome, {loggedInUser}!</p>
+                        <button onClick={handleLogout}>Logout</button>
+                    </>
+                )}
+            </header>
+        </div>
+    );
 }
 
 export default App;
