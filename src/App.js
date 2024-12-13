9aec10670b01db3b888d6234686b154beb091108
Bala <balakrishna.malipeddi@gmail.com>
2024-12-13 14:07:53 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './App.css';
-import LoginForm from './components/LoginForm';
 function App() {
     const [loggedInUser, setLoggedInUser] = useState(null);
 
@@ -14,15 +13,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>myshow4all</h1>
-                {!loggedInUser ? (
-                    <LoginForm setLoggedInUser={setLoggedInUser} />
-                ) : (
-                    <>
-                        <p>Welcome, {loggedInUser}!</p>
-                        <button onClick={handleLogout}>Logout</button>
-                    </>
-                )}
+                
             </header>
         </div>
     );
